cd0f336bc00490fa376bbb064487180e1b5312c9
MOSS17 <42181277+MOSS17@users.noreply.github.com>
2021-03-20 00:30:25 -0700

    Publish project

---
+++ Source/UnrealStarterProject/UnrealStarterProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealStarterProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTARTERPROJECT_API AUnrealStarterProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
